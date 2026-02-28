424d3451b94c7316e006911d6a008edcfb704a7f
Hamza El arji <hamza.elarji@telecom-paris.fr>
2026-02-28 14:57:38 +0000

    maj

--- algothon/bot/run.py
+++ algothon/bot/run.py
@@ -8,8 +8,8 @@ from bot import AlgothonBot
 # ── Config ────────────────────────────────────────────────────────────────────
 
 EXCHANGE_URL    = "http://ec2-52-49-69-152.eu-west-1.compute.amazonaws.com/"
-USERNAME        = "testhamza"
-PASSWORD        = "testhamza"
+USERNAME        = "testhamza1"
+PASSWORD        = "testhamza1"
 AERODATABOX_KEY = "0a7ff9f16fmsh54fb7da32af7310p12b89fjsn800a543a8628"
 
 # London is UTC+0 in winter (GMT), UTC+1 in summer (BST)
